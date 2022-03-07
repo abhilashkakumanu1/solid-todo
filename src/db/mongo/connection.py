21983eff8fe70787e376f294197d33bb98bcfec2
abhilashkakumanu1 <abhilashkakumanu1@gmail.com>
2022-03-07 17:08:44 +0530

    added logging in db.connection.py file

--- src/db/mongo/connection.py
+++ src/db/mongo/connection.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from dotenv import load_dotenv
 from mongoengine import (
@@ -15,6 +16,7 @@ connection_str = os.environ.get("DB_CONNECTION_STRING")
 class DBImp(IDB):
     def connect(self):
         connect(host=connection_str)
+        logging.info("successfully connected to DB")
 
 
 DB = DBImp()
