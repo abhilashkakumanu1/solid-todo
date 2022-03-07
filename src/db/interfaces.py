4379dda1f8869cacab86739aaa61babf411ed431
abhilashkakumanu1 <abhilashkakumanu1@gmail.com>
2022-03-07 16:38:44 +0530

    add db interfaces

---
+++ src/db/interfaces.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+from todo import Todo
+from todo_list import TodoList
+
+
+class IDB(ABC):
+    @abstractmethod
+    def connect():
+        """Connect to Database"""
+
+
+class ITodoListRepo(ABC):
+    @abstractmethod
+    def get(self, id: str) -> TodoList:
+        """Get the todo-list with given id"""
+
+    @abstractmethod
+    def create(self, todo_list: TodoList):
+        """Create new todo-list with given name"""
+
+    @abstractmethod
+    def add_todo(self, todo_list_id: str, todo: Todo):
+        """add todo to the todo-list with given id"""
+
+    @abstractmethod
+    def toggle_todo(self, todo_list_id: str, todo_id: str):
+        """toogle is_completed field of the given todo"""
+
+    @abstractmethod
+    def update_task(self, todo_list_id: str, todo_id: str, task: str):
+        """update task details of the given todo"""
+
+    def delete_todo(self, todo_list_id: str, todo_id: str):
+        """Delete a todo with given id from todo-list"""
